2ca43aeafbed1393159d64d79ba6a20eec16f669
Sebastian <poveda1999@gmail.com>
2021-06-27 22:33:14 -0500

    probando

--- .nuxt/dist/server/server.js
+++ .nuxt/dist/server/server.js
@@ -840,7 +840,7 @@ async function setContext(app, context) {
   // If context not defined, create it
   if (!app.context) {
     app.context = {
-      isStatic: false,
+      isStatic: true,
       isDev: false,
       isHMR: false,
       app,
@@ -849,14 +849,6 @@ async function setContext(app, context) {
       base: app.router.options.base,
       env: {}
     }; // Only set once
-
-    if (context.req) {
-      app.context.req = context.req;
-    }
-
-    if (context.res) {
-      app.context.res = context.res;
-    }
 
     if (context.ssrContext) {
       app.context.ssrContext = context.ssrContext;
@@ -2054,6 +2046,16 @@ const layouts = {
 
   async mounted() {
     this.$loading = this.$refs.loading;
+
+    if (this.isPreview) {
+      if (this.$store && this.$store._actions.nuxtServerInit) {
+        this.$loading.start();
+        await this.$store.dispatch('nuxtServerInit', this.context);
+      }
+
+      await this.refresh();
+      this.$loading.finish();
+    }
   },
 
   watch: {
@@ -2066,6 +2068,10 @@ const layouts = {
 
     isFetching() {
       return this.nbFetching > 0;
+    },
+
+    isPreview() {
+      return Boolean(this.$options.previewData);
     }
 
   },
@@ -2158,6 +2164,62 @@ const layouts = {
       }
 
       return Promise.resolve(layouts['_' + layout]);
+    },
+
+    getRouterBase() {
+      return Object(external_ufo_["withoutTrailingSlash"])(this.$router.options.base);
+    },
+
+    getRoutePath(route = '/') {
+      const base = this.getRouterBase();
+      return Object(external_ufo_["withoutTrailingSlash"])(Object(external_ufo_["withoutBase"])(Object(external_ufo_["parsePath"])(route).pathname, base));
+    },
+
+    getStaticAssetsPath(route = '/') {
+      const {
+        staticAssetsBase
+      } = window.__NUXT__;
+      return urlJoin(staticAssetsBase, this.getRoutePath(route));
+    },
+
+    async fetchStaticManifest() {
+      return window.__NUXT_IMPORT__('manifest.js', Object(external_ufo_["normalizeURL"])(urlJoin(this.getStaticAssetsPath(), 'manifest.js')));
+    },
+
+    setPagePayload(payload) {
+      this._pagePayload = payload;
+      this._fetchCounters = {};
+    },
+
+    async fetchPayload(route, prefetch) {
+      const path = Object(external_ufo_["decode"])(this.getRoutePath(route));
+      const manifest = await this.fetchStaticManifest();
+
+      if (!manifest.routes.includes(path)) {
+        if (!prefetch) {
+          this.setPagePayload(false);
+        }
+
+        throw new Error(`Route ${path} is not pre-rendered`);
+      }
+
+      const src = urlJoin(this.getStaticAssetsPath(route), 'payload.js');
+
+      try {
+        const payload = await window.__NUXT_IMPORT__(path, Object(external_ufo_["normalizeURL"])(src));
+
+        if (!prefetch) {
+          this.setPagePayload(payload);
+        }
+
+        return payload;
+      } catch (err) {
+        if (!prefetch) {
+          this.setPagePayload(false);
+        }
+
+        throw err;
+      }
     }
 
   },
@@ -2494,7 +2556,11 @@ const createNext = ssrContext => opts => {
     routePath: ''
   };
   ssrContext.fetchCounters = {}; // Remove query from url is static target
-  // Public runtime config
+
+  if (ssrContext.url) {
+    ssrContext.url = ssrContext.url.split('?')[0];
+  } // Public runtime config
+
 
   ssrContext.nuxt.config = ssrContext.runtimeConfig.public;
 
